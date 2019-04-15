5a1c3aaf8178bd12d4ebcf593c1796b444effc92
Ivan Szebenszki <szib77@gmail.com>
2019-04-15 15:18:11 +0100

    Remove random seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,29 @@
 Faker::Config.random = Random.new(42)
 Faker::Config.locale = 'en-GB'
 
-num_of_admins = 5
-num_of_guests = 50
-num_of_events = 30
-num_of_attendance = 30
+num_of_admins = 0
+num_of_guests = 0
+num_of_events = 0
+num_of_attendance = 0
+
+admin = Admin.create(name: 'Admin')
+ivan = Guest.create(name: 'Ivan')
+rebecca = Guest.create(name: 'Rebecca')
+e1 = Event.create(title: 'Title 1', venue: 'V1', date: Faker::Date.forward(30), admin: admin)
+e2 = Event.create(title: 'Title 2', venue: 'V1', date: Faker::Date.forward(30), admin: admin)
+e3 = Event.create(title: 'Title 3', venue: 'V2', date: Faker::Date.forward(30), admin: admin)
+e4 = Event.create(title: 'Title 4', venue: 'V2', date: Faker::Date.forward(30), admin: admin)
+a1 = Attendance.create(event: e1, guest: ivan)
+a2 = Attendance.create(event: e2, guest: ivan)
+a3 = Attendance.create(event: e1, guest: rebecca)
+a4 = Attendance.create(event: e3, guest: rebecca)
 
 #  --- DO NOT EDIT BELOW THIS LINE ---
 num_of_admins.times { Admin.create(name: Faker::Name.unique.name) }
 num_of_guests.times { Guest.create(name: Faker::Name.unique.name_with_middle) }
 
 num_of_events.times do
-  admin = Admin.find(rand(1..num_of_admins))
+  # admin = Admin.find(rand(1..num_of_admins))
   Event.create(
     title: "#{admin.name}'s #{Faker::Verb.ing_form} event",
     description: Faker::Lorem.sentences(3).join(' '),
