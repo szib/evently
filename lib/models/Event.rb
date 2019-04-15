956a4bc44e12d945026f894f8a9c6d8400134d04
Ivan Szebenszki <szib77@gmail.com>
2019-04-15 16:52:37 +0100

    Add Event#display

--- lib/models/Event.rb
+++ lib/models/Event.rb
@@ -8,4 +8,14 @@ class Event < ActiveRecord::Base
   has_many :guests, through: :attendances
 
   validates :admin, :title, :date, :venue, presence: true
+
+  def display
+    pastel = Pastel.new
+    puts '-' * 40
+    puts pastel.yellow(title)
+    puts '-' * 40
+    puts description
+    puts pastel.cyan("\tDate:\t #{date}")
+    puts pastel.cyan("\tVenue:\t #{venue}")
+  end
 end
