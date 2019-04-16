384860028669ae0ba86e426463b5bf25ec54b676
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-16 13:15:28 +0100

    Add parse_event_information method for API class

--- db/seed/seed_with_rest_client.rb
+++ db/seed/seed_with_rest_client.rb
@@ -3,5 +3,8 @@
 # ========================================
 
 def seed_with_rest_client
-  puts 'TODO :)'
+  api = API.new
+  raw_event_data = api.get_flatiron_events
+  event_data = api.parse_event_information(raw_event_data)
+  #iterate over event_data and call Event.create
 end
--- lib/API.rb
+++ lib/API.rb
@@ -11,4 +11,20 @@ class API
     hash["events"]
   end
 
+  def parse_event_information(raw_event_data)
+    event_data = []
+    raw_event_data.each do |event|
+      event_hash = {}
+
+      event_hash[:title] = event["name"]["text"]
+      event_hash[:description] = event["description"]["text"]
+      event_hash[:date] = event["start"]["local"]
+      event_hash[:venue_id] = event["venue_id"]
+
+      event_data << event_hash
+    end
+    binding.pry
+    event_data
+  end
+
 end
