5d578a1ac0f10c958f128a80fe3c389720186689
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-16 09:57:01 +0100

    Add guest name with friends method for Attendance class

--- lib/models/Attendance.rb
+++ lib/models/Attendance.rb
@@ -16,6 +16,13 @@ class Attendance < ActiveRecord::Base
     # returns e.g "Name + 5 friends"
     # returns e.g "Name + a friend"
     # returns e.g "Name" if no friends
+    if self.num_of_extra_guests == 0
+      return "#{self.guest.name}"
+    elsif self.num_of_extra_guests == 1
+      return "#{self.guest.name} + a friend"
+    else
+      return "#{self.guest.name} + #{self.num_of_extra_guests} friends"
+    end
   end
 
   def self.toggle_attendance(event:, user:)
