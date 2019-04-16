34143bd8f05cc9d64446c90768517860e65b7775
Ivan Szebenszki <szib77@gmail.com>
2019-04-16 18:02:36 +0100

    Require dotenv/load

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require 'dotenv/load'
+
 connection_details = YAML.safe_load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
 require_all 'lib'
