c94945f009473739b4c1f31c6d0cdc38407b29ce
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-16 13:15:28 +0100

    Add venue name from Eventbrite API

--- lib/API.rb
+++ lib/API.rb
@@ -11,6 +11,15 @@ class API
     hash["events"]
   end
 
+  def get_venue_name(venue_id)
+    venue_url = "https://www.eventbriteapi.com/v3/venues/#{venue_id}/?token=#{ENV["EVENTBRITE_API_KEY"]}"
+    response = RestClient.get(venue_url)
+    hash = JSON.parse(response)
+    venue_address = hash["address"]["localized_address_display"]
+    venue_name = hash["name"]
+    "#{venue_name}, #{venue_address}"
+  end
+
   def parse_event_information(raw_event_data)
     event_data = []
     raw_event_data.each do |event|
@@ -19,7 +28,7 @@ class API
       event_hash[:title] = event["name"]["text"]
       event_hash[:description] = event["description"]["text"]
       event_hash[:date] = event["start"]["local"]
-      event_hash[:venue] = event["venue_id"]
+      event_hash[:venue] = get_venue_name(event["venue_id"])
       event_hash[:admin_id] = 1
 
       event_data << event_hash
