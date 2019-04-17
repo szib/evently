1ef471905ed2656c9d88b71e1248dc803cd079fe
Ivan Szebenszki <szib77@gmail.com>
2019-04-17 21:47:46 +0100

    Refactor API

--- db/seed/seed_with_rest_client.rb
+++ db/seed/seed_with_rest_client.rb
@@ -3,13 +3,8 @@
 # ========================================
 
 def seed_with_rest_client
-  api = API.new
-  if api.api_key_present?
-    raw_event_data = api.get_flatiron_events
-    event_data = api.parse_event_information(raw_event_data)
-    event_data.each do |event|
-      Event.create(event)
-    end
+  if API.api_key_present?
+    API.import_events
   else
     puts "No API key detected. Quiting..."
   end
--- lib/API.rb
+++ lib/API.rb
@@ -1,21 +1,29 @@
 class API
   URL = "https://www.eventbriteapi.com/v3/events/search/?organizer.id=17393370241"
 
-  def api_key_present?
+  def self.api_key_present?
     ENV.key?("EVENTBRITE_API_KEY")
   end
 
-  def url_with_token
+  def self.import_events
+    raw_event_data = retrive_event_data_from_eventbrite
+    event_data = parse_event_information(raw_event_data)
+    save_to_database(event_data)
+  end
+  
+  private
+
+  def self.url_with_token
     "#{URL}&token=#{ENV["EVENTBRITE_API_KEY"]}"
   end
 
-  def get_flatiron_events
+  def self.retrive_event_data_from_eventbrite
     response = RestClient.get(url_with_token)
     hash = JSON.parse(response)
     hash["events"]
   end
 
-  def get_venue_name(venue_id)
+  def self.get_venue_name(venue_id)
     venue_url = "https://www.eventbriteapi.com/v3/venues/#{venue_id}/?token=#{ENV["EVENTBRITE_API_KEY"]}"
     response = RestClient.get(venue_url)
     hash = JSON.parse(response)
@@ -24,7 +32,7 @@ class API
     "#{venue_name}, #{venue_address}"
   end
 
-  def parse_event_information(raw_event_data)
+  def self.parse_event_information(raw_event_data)
     event_data = []
     raw_event_data.each do |event|
       event_hash = {}
@@ -38,5 +46,10 @@ class API
     end
     event_data
   end
+
+  def self.save_to_database(data)
+    data.each { |event| Event.create(event) }
+  end
+
 
 end
